57d214df22d5f86353b524f84fa018b32481c17c
bill bittner <bittner.w@gmail.com>
2017-03-08 16:21:59 -0800

    Revert "Revert "configured server.js""
    Reverting
    
    This reverts commit 3150fff13b31baa01546c7d182ae2da51e33fdba.
    sd

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
+// Load dependencies.
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./config");
 
 // boilerplate 
+const app = express();
 
+app.use(express.static("./public"));
 
+app.use(bodyParser.urlencoded({ extended: false}));
 
-// main routes 
-
+// server-side route that directs http routes back to the react app
+app.get("/*", function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 
 // catch all route 
 Router.get("*", function(req, res){
     res.sendFile(__dirname + "/public/index.html");
 });
+
+const PORT = process.env.PORT || 3000;
+
+// start the server
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:", PORT);
+})
